03b06c6c2c5001dd874cdb5c2f7056b4224ba213
snorkypie <>
2023-05-31 22:58:46 +0300

    Remove import of hardhat/console.sol

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -3,7 +3,6 @@ pragma solidity =0.8.19;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "hardhat/console.sol";
 
 /// @title Vesting for Team and Advisory KERC tokens
 /// @author snorkypie
